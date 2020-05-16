36fe663598a3a748313f06f6eeee38ffdad3265d
taoli <15230817603@163.com>
2020-05-16 21:26:13 +0800

    列表渲染

--- react-demo/src/components/listRender/index.js
+++ react-demo/src/components/listRender/index.js
@@ -1,32 +1,34 @@
 import React from 'react'
 
 
-export default class List extends React.Component{
-    constructor(props){
+export default class List extends React.Component {
+    constructor(props) {
         super(props)
         this.state = {
-            list:[
-                {title:'第一节课',content:'aa'},
-                {title:'第二节课',content:'bb'},
-                {title:'第三节课',content:'cc'},
-                {title:'第四节课',content:'dd'},
+            list: [
+                { title: '第一节课', content: 'aa' },
+                { title: '第二节课', content: 'bb' },
+                { title: '第三节课', content: 'cc' },
+                { title: '第四节课', content: 'dd' },
             ]
         }
     }
 
-    render(){
+    render() {
         return (
             <div>
-                {
-                    this.state.list.map((item,index)=>{
-                        return (
-                            <li key={index}>
-                                <h3>{item.title}</h3>
-                                <p>{item.content}</p>
-                            </li>
-                        )
-                    })
-                }
+                <ul>
+                    {
+                        this.state.list.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    <h3>{item.title}</h3>
+                                    <p>{item.content}</p>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
             </div>
         )
     }
